c3ffe538685e421bb75e7d5911ebc4d59477b094
avneeetk <avneet15khanna@gmail.com>
2024-08-03 01:08:32 +0530

    index2

--- index.js
+++ index.js
@@ -7,20 +7,29 @@ app.use(express.json());
 
 const usersFilePath = path.join(__dirname, 'data', 'users.json');
 
+// Helper function to read users from file
 const getUsers = () => {
   const data = fs.readFileSync(usersFilePath);
   return JSON.parse(data);
 };
 
+// Helper function to write users to file
 const saveUsers = (users) => {
   fs.writeFileSync(usersFilePath, JSON.stringify(users, null, 2));
 };
 
+// Route for the root path
+app.get('/', (req, res) => {
+  res.send('Welcome to the demo app');
+});
+
+// Route to get users
 app.get('/users', (req, res) => {
   const users = getUsers();
   res.json(users);
 });
 
+// Route to add a new user
 app.post('/users', (req, res) => {
   const users = getUsers();
   const newUser = { id: Date.now(), ...req.body };
